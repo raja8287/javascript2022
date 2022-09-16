45d24b065d67e3c8c925b843ba28bd449ab9fb66
raja <rdas828726@gmail.com>
2022-09-16 23:16:01 +0530

    __

---
+++ web_page01.js
@@ -0,0 +1,10 @@
+function c(){
+    var x=document.getElementById("list");
+    if(x.style.display=="block"){
+        x.style.display="none";
+        
+    }
+    else{
+        x.style.display="block";
+    }
+}
